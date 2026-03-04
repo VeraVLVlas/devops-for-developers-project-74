e08130200a107c746b50efcaf5324eb011b9f8c5
VeraVLVlas <vlasovavery@gmail.com>
2026-03-04 17:16:02 +0100

    feat: Perform the third step.
    -  Add Dokerfile Production and publish the project on Docker Hub.

--- make-compose.mk
+++ make-compose.mk
@@ -5,4 +5,4 @@ compose-up:
 	docker compose up
 
 compose-test:
-	docker-compose -f docker-compose.yml up --abort-on-container-exit --exit-code-from app
+	docker compose -f docker-compose.yml up --abort-on-container-exit
